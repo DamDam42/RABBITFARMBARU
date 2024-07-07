bd4928e96276681f408ba1397f4a2003bdf0e98d
DamDam42 <2022464546@student.uitm.edu.my>
2024-07-08 01:23:25 +0800

    a

--- src/main/java/com/heroku/java/controller/PaymentController.java
+++ src/main/java/com/heroku/java/controller/PaymentController.java
@@ -150,6 +150,7 @@ public class PaymentController {
                 
                 
             } catch (SQLException e) {
+                e.printStackTrace();
             } return "Payment/StaffViewPayment";
         }
 
